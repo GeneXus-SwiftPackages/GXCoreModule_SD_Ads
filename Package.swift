675962d866d3141d6825b6f537316a08522f47d3
Build Er <builder@macmini6.local>
2023-03-16 14:53:10 -0300

    Package version: 1.0.0-beta+20230316174639

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
 		.target(name: "GXCoreModule_SD_AdsWrapper",
 				dependencies: [
 					"GXCoreModule_SD_Ads",
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .tvOS]))
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.tvOS, .iOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Ads",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Ads-1.0.0-beta+20230316142352.xcframework.zip",
-			checksum: "4a88f81573088cd0845e52a9634366238877ad9243011b15e4915c106e6abe8f"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Ads-1.0.0-beta+20230316174639.xcframework.zip",
+			checksum: "e5fe2dfb30bee9cf950e4ad839a90ad60be58e82055bc86d4ce4ebc485a9bb01"
 		)
 	]
 )
